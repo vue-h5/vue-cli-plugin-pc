9c06f40005a707a9d2e0e25515feca6642f5528e
ektx <530675800@qq.com>
2019-04-26 09:18:59 +0800

    添加svg icon模板

---
+++ generator/template/src/components/svgIcon/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg><use :xlink:href="iconName"/></svg>
+</template>
+
+<script>
+export default {
+    name: 'svg-icon-com',
+    props: {
+        // icon 接受在 assets/icon/svg 中的文件名
+        icon: {
+            type: String,
+            default: ''
+        }
+    },
+    computed: {
+        iconName () {
+            return `#svg-icon-${this.icon}`
+        }
+    }
+}
+</script>
